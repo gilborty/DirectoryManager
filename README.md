a38f2f444d3e18e1a5b963cc48b9a06f67b5c7fc
Gilbert Montague <nerdgilbert@users.noreply.github.com>
2016-03-07 23:35:13 -0500

    Added a readme lol

---
+++ README.md
@@ -0,0 +1,17 @@
+Silly test program to delete files from a directory at a certain rate.
+
+Probably not safe.
+
+To run, do the following:
+
+mkdir build
+
+cd build
+
+make
+
+cp ../tests/CreateDummyFiles.sh (might have to chmod it)
+
+and then run both the script and the program.
+
+Have fun.
--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,9 @@ std::vector<std::string> OpenDir( std::string path )
 {
     DIR *dir;
     dirent *pDir;
-    
+
     std::vector<std::string> files;
-    
+
     dir = opendir( path.c_str() );
     if( dir == NULL )
     {
@@ -34,15 +34,15 @@ std::vector<std::string> OpenDir( std::string path )
         std::vector<std::string> nullString;
         return nullString;
     }
-        
+
     while( (pDir = readdir( dir )) != NULL )
     {
         files.push_back(pDir->d_name);
     }
-    
+
     closedir(dir);
     return files;
-    
+
 }
 
 void AddToMap( std::pair<std::string, time_t> pairIn )
@@ -64,15 +64,15 @@ void AddToMap( std::pair<std::string, time_t> pairIn )
     }
     //std::cout << "Adding file: " << pairIn.first << std::endl;
     filesToBeDeleted.insert( pairIn );
-}    
-    
+}
+
 void ClearMemory()
 {
         std::cout << "Clear Memory called." << std::endl;
         std::vector<std::string> filesToRemoveFromMap;
-        
+
         auto timeNow = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        
+
         auto compareTime = timeNow - secondsUntilDelete;
         std::cout << "Files older than: " << std::ctime(&compareTime) << std::endl;
     for( auto file : filesToBeDeleted )
@@ -83,12 +83,12 @@ void ClearMemory()
             //std::cout << file.first << " is old" << std::endl;
             std::string fullPath = databaseDirectory + file.first;
             remove( fullPath.c_str() );
-            
+
             //Set remove it from the map
             filesToRemoveFromMap.push_back(file.first);
         }
-    } 
-    
+    }
+
     //Remove values from map
     for( auto value : filesToRemoveFromMap )
     {
@@ -108,7 +108,7 @@ void CheckTime()
     std::time_t reportTime = std::chrono::system_clock::to_time_t(currentTime);
     std::cout << "The time is: " << std::ctime( &reportTime ) << std::endl;
 }
-    
+
 
 int main( int argc, char* argv[] )
 {
@@ -119,49 +119,48 @@ int main( int argc, char* argv[] )
         //CheckTime();
 
         std::vector<std::string> files;
-        files = OpenDir( argv[1] );
-        
+        files = OpenDir( databaseDirectory.c_str() );
+
         for( auto& element : files )
         {
             //Check for top level and current level directories
-            //If they are files we want to look at, proceed   
+            //If they are files we want to look at, proceed
             if( element.compare( 0, 1, "." ) != 0 )
             {
-        
+
                 std::string fullPath = databaseDirectory + element;
-            
+
                 struct stat buffer;
                 stat( fullPath.c_str(), &buffer );
-                
+
                 int memorySize = buffer.st_size;
                 time_t timeAccessed = buffer.st_ctime;
-               
+
                 auto pair = std::make_pair(element, timeAccessed);
                 AddToMap( pair );
-                
-                memoryUsed = memoryUsed + memorySize;            
+
+                memoryUsed = memoryUsed + memorySize;
              }
         }
-        
-        std::cout << "Memory in " << databaseDirectory << ": " << memoryUsed/1000000 << " MB" << std::endl; 
-        
+
+        std::cout << "Memory in " << databaseDirectory << ": " << memoryUsed/1000000 << " MB" << std::endl;
+
         if( memoryUsed > memoryLimit )
         {
             std::cout << "MEMORY LIMIT EXCEEDED!" << std::endl;
             ClearMemory();
         }
-        
+
         SleepFor( 100 );
-    
+
         memoryUsed = 0;
         auto nowTime = std::chrono::system_clock::now();
         std::chrono::duration<double> elapsedSeconds = nowTime - startTime;
         std::cout << "Uptime: " << elapsedSeconds.count() << std::endl;
         std::cout << std::endl;
     }
-    
-    
+
+
     return 0;
 
 }
-
